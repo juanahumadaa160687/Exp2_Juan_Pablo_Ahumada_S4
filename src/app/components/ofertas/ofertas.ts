623c1beea5457532fca14a0069a9da5cead5f191
Juan Pablo Ahumada <jua.ahumadaa@duocuc.cl>
2026-06-15 15:35:00 -0400

    Corrección de links

--- src/app/components/ofertas/ofertas.ts
+++ src/app/components/ofertas/ofertas.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NavbarComponent} from '../navbar/navbar';
+import {Footer} from '../footer/footer';
 
 @Component({
   selector: 'app-ofertas',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    Footer
+  ],
   templateUrl: './ofertas.html',
   styleUrl: './ofertas.css',
 })
